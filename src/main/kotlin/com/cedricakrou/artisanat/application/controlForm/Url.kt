df72eb50969c7616127d1b8c6c3858aa2f25b239
cedricakrou <cedricakroustudy@gmail.com>
2021-12-06 22:52:49 +0000

    Feature: readme

--- src/main/kotlin/com/cedricakrou/artisanat/application/controlForm/Url.kt
+++ src/main/kotlin/com/cedricakrou/artisanat/application/controlForm/Url.kt
@@ -2,7 +2,7 @@ package com.cedricakrou.artisanat.application.controlForm
 
 object Url {
 
-    const val BASE_URL = "http://192.168.186.206:8080"
+    const val BASE_URL = "http://127.0.0.1:8080"
 //    const val BASE_URL = "http://vps-d52f94f7.vps.ovh.net:8080/account/login"
 //    const val BASE_URL = "http://vps-d52f94f7.vps.ovh.net:8080"
 }
